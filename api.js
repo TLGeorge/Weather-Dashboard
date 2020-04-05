024572c8b5229e277cf592bbd55677ca73aee722
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-04-04 22:32:59 -0400

    update to index and api script

--- api.js
+++ api.js
@@ -7,10 +7,40 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&
 
 
 // We then created an AJAX call
+
 $.ajax({
     url: queryURL,
     method: "GET"
 }).done(function (response) {
     console.log(response);
-    $("#temp").append(response.list[0].main.temp);
+    // $("#temp").append(response.list[0].main.temp);
+    // $("#humidity").append();
+    // $("#wind").append();
+    // $("#uvi").append();
+
+    // // DAY 1 of Forecast
+    // $("date1").append();
+    // $("weathericon1").append();
+    // $("temperature1").append(response.list[1].main.temp);
+    // $("humidity1").append();
+    // // Day 2 of Forecast
+    // $("date2").append();
+    // $("weathericon2").append();
+    // $("temperature2").append(response.list[2].main.temp);
+    // $("humidity2").append();
+    // // Day 3 of Forecast
+    // $("date3").append();
+    // $("weathericon3").append();
+    // $("temperature3").append(response.list[3].main.temp);
+    // $("humidity3").append();
+    // // Day 4 of Forecast
+    // $("date4").append();
+    // $("weathericon4").append();
+    // $("temperature4").append(response.list[4].main.temp);
+    // $("humidity4").append();
+    // // Day 5 of Forecast
+    // $("date5").append();
+    // $("weathericon5").append();
+    // $("temperature5").append(response.list[5].main.temp);
+    // $("humidity5").append();
 })
